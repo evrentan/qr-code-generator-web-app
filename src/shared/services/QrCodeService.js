a4d5b0922f06bedb50cb6dd7788919655083996f
Evren Tan <evrentan@gmail.com>
2022-06-24 12:25:26 +0300

    Update proxy

--- src/shared/services/QrCodeService.js
+++ src/shared/services/QrCodeService.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
+const qrCodeBackendUrl = 'https://qr-code-generator-backend.herokuapp.com'
 const generateQrCodeBaseUrl = '/qrCode/generate';
 const generateQrCodeString = '/qrCodeString'
 
 export const QrCodeService = {
 
     generateQrCode(request) {
-        return axios.post(generateQrCodeBaseUrl + generateQrCodeString,
+        return axios.post(qrCodeBackendUrl + generateQrCodeBaseUrl + generateQrCodeString,
             request);
     },
 };
